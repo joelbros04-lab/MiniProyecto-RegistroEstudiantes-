553de649f576735f1a7a12c72f8c1b06412db0c7
Joel <joelbros04@gmail.com>
2026-05-22 20:02:25 -0500

    agregar registro de estudiantes

--- Estudiantes.cpp
+++ Estudiantes.cpp
@@ -14,6 +14,52 @@ void mostrarMenu() {
 
     cout << "Seleccione una opcion: ";
 }
+const int MAX = 50;
+
+string nombres[MAX];
+int edades[MAX];
+double notas[MAX];
+
+int cantidad = 0;
+
+void registrarEstudiante(
+    string nombres[],
+    int edades[],
+    double notas[],
+    int &cantidad
+) {
+
+    cout << "\n=== REGISTRAR ESTUDIANTE ===" << endl;
+
+    cout << "Ingrese nombre: ";
+    cin >> nombres[cantidad];
+
+    cout << "Ingrese edad: ";
+    cin >> edades[cantidad];
+
+    // Validacion edad
+    while(edades[cantidad] <= 0) {
+
+        cout << "Edad invalida. Ingrese nuevamente: ";
+        cin >> edades[cantidad];
+    }
+
+    cout << "Ingrese nota: ";
+    cin >> notas[cantidad];
+
+    // Validacion nota
+    while(notas[cantidad] < 0 || notas[cantidad] > 20) {
+
+        cout << "Nota invalida. Ingrese nuevamente: ";
+        cin >> notas[cantidad];
+    }
+
+    // IMPORTANTE
+    // aumenta la cantidad de estudiantes
+    cantidad++;
+
+    cout << "Estudiante registrado correctamente." << endl;
+}
 
 int main() {
 
@@ -28,7 +74,7 @@ int main() {
         switch(opcion) {
 
             case 1:
-                cout << "Opcion registrar estudiante" << endl;
+                registrarEstudiante(nombres, edades, notas, cantidad);
                 break;
 
             case 2:
